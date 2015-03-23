6e562c7479143f96b3be06a96a93bde4e40edc79
Peter Eichman <peichman@umd.edu>
2015-03-23 14:44:13 -0400

    Use the overlay project fcrepo WAR.
    Assume the WAR file to deploy is located in /vagrant.

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,6 @@ chown tomcat:tomcat "$FEDORA_HOME"
 # Tomcat configuration
 cp /vagrant/etc/tomcat/* /etc/tomcat
 
-cp /apps/git/fcrepo4/fcrepo-webapp/target/fcrepo-webapp-4.0.0.war /var/lib/tomcat/webapps
+cp /vagrant/um-fcrepo-webapp-4.1.1-SNAPSHOT.war /var/lib/tomcat/webapps/fcrepo.war
 
 service tomcat restart
