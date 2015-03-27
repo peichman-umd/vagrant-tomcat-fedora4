fc095847130fda7f0dcaac6d5786bcbb0e97e9b7
Peter Eichman <peichman@umd.edu>
2015-03-27 11:59:32 -0400

    Set a root ACL when provisioning.
    Set the ACL of the root /rest resource to give everyone read-only access. Added a user named "user" with the container role of fedoraUser, to allow for "public" login.

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,16 @@ cp /vagrant/um-fcrepo-webapp-4.1.1-SNAPSHOT.war /var/lib/tomcat/webapps/fcrepo.w
 
 service tomcat restart
 service httpd restart
+
+# give Tomcat time to start up
+sleep 10
+
+# set up read-only access for all authenticated users
+# note the double slash in rest//fcr:accessroles
+# this is NOT a typo, it is require to set the root
+# collection's ACL
+curl -s http://localhost:8080/fcrepo/rest//fcr:accessroles \
+    -X POST \
+    -u admin:admin \
+    -H 'Content-Type: application/json' \
+    -d '{"EVERYONE":["reader"]}'
